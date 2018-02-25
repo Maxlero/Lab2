73eafe2e391cda911c5c279941e4b64d32759ef4
Maxlero <Maxlero@yandex.ru>
2018-02-25 16:58:08 +0300

    done

--- main.py
+++ main.py
@@ -32,6 +32,10 @@ def f(x):
     return x - np.log(x - 1 + ((x - 1) ** 2 + 1) ** 0.5)
 
 
+def diff_f(point):
+    return derivative(f, point, dx=1e-5)
+
+
 def diff_2_f(point):
     return derivative(f, point, dx=1e-5, n=2)
 
@@ -85,6 +89,59 @@ def chords_method(left, right, motionless):
                   " <= " + str(accuracy))
 
 
+def max_diff(a, b):
+    step = 0.01  # шаг
+    max_value = 0  # максимальное значение
+    array = np.arange(a, b + step, step)
+    for i in array:
+        if abs(diff_f(i)) > abs(max_value):
+            max_value = diff_f(i)
+    return max_value
+
+
+def g(x, l):
+    return x - l * f(x)
+
+
+def aitken_method(x0, l):
+    global iterations
+    global accuracy
+
+    if f(x0) != 0 and abs(f(x0)) > accuracy:
+        iterations += 1
+        x1 = g(x0, l)
+        x2 = g(x1, l)
+        x2s = (x0 * x2 - x1 ** 2) / (x2 - 2 * x1 + x0)
+        x3 = g(x2s, l)
+        if f(x3) != 0 and abs(f(x3)) > accuracy:
+            aitken_method(x2s, l)
+        else:
+            print("Корень: " + str("%.9f" % x3))
+            print("Количество итераций: " + str(iterations))
+            print("Проверка: f(x) = " + str("%.2e" % f(x3)) +
+                  " <= " + str(accuracy))
+    else:
+        print("Корень: " + str("%.9f" % x0))
+        print("Количество итераций: " + str(iterations))
+        print("Проверка: f(x) = " + str("%.2e" % f(x0)) +
+              " <= " + str(accuracy))
+
+
+def steffensen_method(x0):
+    global iterations
+    global accuracy
+
+    if f(x0) != 0 and abs(f(x0)) > accuracy:
+        iterations += 1
+        x1 = x0 - ((f(x0)) ** 2) / (f(x0) - f(x0 - f(x0)))
+        steffensen_method(x1)
+    else:
+        print("Корень: " + str("%.9f" % x0))
+        print("Количество итераций: " + str(iterations))
+        print("Проверка: f(x) = " + str("%.2e" % f(x0)) +
+              " <= " + str(accuracy))
+
+
 if __name__ == "__main__":
     # TODO rename files
     ####################################################################################################################
@@ -123,3 +180,24 @@ if __name__ == "__main__":
     print("___________________________________________________________________________________________________________")
     print("Метод Эйткена:")
     ####################################################################################################################
+    try:
+        # нарисуем 1-ю производную на [a, b]
+        draw(diff_f, a, b, f(a), f(b), a, b)
+
+        # найдем l = 1 / M = 1 / max|f'(x)| on [a, b]
+        l = 1 / max_diff(a, b)
+
+        iterations = 0
+        aitken_method(x0, l)
+    except:
+        print("Что-то пошло не так.. Возможно, на промежутке несколько корней.")
+    ####################################################################################################################
+    print("___________________________________________________________________________________________________________")
+    print("Метод Стеффенсона:")
+    ####################################################################################################################
+    try:
+        # этот метод - комбинация 2-х предыдущих методов
+        iterations = 0
+        steffensen_method(x0)
+    except:
+        print("Что-то пошло не так.. Возможно, на промежутке несколько корней.")
